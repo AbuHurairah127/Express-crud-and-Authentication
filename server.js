bf3dab5986c6a3e642747b13ab3e1af2e0a465ae
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2022-08-21 23:57:56 +0500

    server is working

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const CONNECTION_URL =
-  "mongodb+srv://abuhurairah_127:abuhurairah_127@cluster0.k8i65fj.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 5000;
-mongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
-  )
-  .catch((error) => console.error(error.message));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
